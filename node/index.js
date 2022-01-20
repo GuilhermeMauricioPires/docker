87f329fa28b80a88bab042d405374137ad0d4166
Guilherme <gmauriciopires@gmail.com>
2022-01-20 00:59:42 -0300

    .

--- node/index.js
+++ node/index.js
@@ -1,24 +1,39 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const config = {
-    host: 'db',
-    user: 'root',
-    password: 'root',
-    database: 'nodedb'
-};
 
-const mysql = require('mysql');
-const connection = mysql.createConnection(config);
+async function createConnection (){
+    const config = {
+        host: 'db',
+        user: 'root',
+        password: 'root',
+        database: 'nodedb'
+    };
+    
+    const mysql = require('mysql');
+    return mysql.createConnection(config);
+}
 
-const sql = "INSERT INTO user (name) VALUES ('Guilherme');";
-connection.query(sql);
-connection.end();
+async function execInsert(){
+    const connection = await createConnection();
+    connection.query("INSERT INTO user (name) VALUES ('Guilherme');");
+    connection.query("INSERT INTO user (name) VALUES ('FullCycle');");
+    connection.end();        
+}
 
-app.get('/', (req, res) => {
-    res.send('<h1>Full Cycle!!!!<h1/>')
+execInsert();
+
+app.get('/', async (req, res) => {
+    const connection = await createConnection();
+    const sql = "SELECT * FROM user;";
+    connection.query(sql, function(err, result, fields){
+        if (err) throw err;
+        let html = '<h1>Full Cycle Rocks!<h1/>';
+        result.map(r => {html += '<h2>' + r.name + '<h2/>'});
+        res.send(html);
+    });
 })
 
-app.listen(port, () => {
-    console.log('Rodando na porta ' + port)
+app.listen(port, async () => {
+    console.log('Rodando na porta ' + port);
 })
