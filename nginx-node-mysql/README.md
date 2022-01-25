6f6aaae4a0bff3da3c87e77453f996079bd75ee8
Guilherme <gmauriciopires@gmail.com>
2022-01-24 23:35:11 -0300

    Ajuste para criar tabela no banco ao iniciar aplicação

--- nginx-node-mysql/README.md
+++ nginx-node-mysql/README.md
@@ -1,3 +1,11 @@
+# Enviado em 21/01/2022 às 16:40 - Correção/Pendente
+
+1 Pra solucionar isto você pode criar a tabela pela aplicação ou usar um script de inicialização do banco de dados. <br/>
+R: Ajustado para criar tabela ao iniciar aplicação.
+
+-----------------------------------------------------------------------------------------------------------------------
+
+
 # Enviado em 20/01/2022 às 23:09 - Correção/Pendente
 
 1 Adicionar o comando 'RUN npm install' no Dockerfile do Node, já que as dependências ainda não estão sendo instaladas. <br/>
--- node/index.js
+++ node/index.js
@@ -3,27 +3,36 @@ const app = express();
 const port = 3000;
 
 async function createConnection (){
+    console.log('createConnection');
     const config = {
         host: 'db',
         user: 'root',
         password: 'root',
-        database: 'nodedb'
+        database: 'nodedb',
+        raise_on_warnings: false
     };
     
     const mysql = require('mysql');
     return mysql.createConnection(config);
 }
 
+async function createTable(){
+    console.log('createTable');
+    const connection = await createConnection();
+    connection.query("CREATE TABLE IF NOT EXISTS user (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255));");
+    connection.end();
+}
+
 async function execInsert(){
+    console.log('execInsert');
     const connection = await createConnection();
     connection.query("INSERT INTO user (name) VALUES ('Guilherme');");
     connection.query("INSERT INTO user (name) VALUES ('FullCycle');");
     connection.end();        
 }
 
-execInsert();
-
 app.get('/', async (req, res) => {
+    await execInsert();
     const connection = await createConnection();
     const sql = "SELECT * FROM user;";
     connection.query(sql, function(err, result, fields){
@@ -35,5 +44,6 @@ app.get('/', async (req, res) => {
 })
 
 app.listen(port, async () => {
+    await createTable();
     console.log('Rodando na porta ' + port);
 })
